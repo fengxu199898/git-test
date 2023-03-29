fb0c4b6abbeec70a300958ae4ea28c9df68b7e6c
fx <648788981@qq.com>
2023-03-29 19:01:25 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/gittest/GitTest.java
+++ src/main/java/com/atguigu/gittest/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!!!");
         System.out.println("agsdag");
         System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix1");
     }
 }
