dd55288d8d83b47f5cc7c97bec631f6e8b474c86
fengxu199898 <92633500+fengxu199898@users.noreply.github.com>
2023-03-29 20:38:44 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/gittest/GitTest.java
+++ src/main/java/com/atguigu/gittest/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello hot-fix1");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("github change test");
     }
 }
